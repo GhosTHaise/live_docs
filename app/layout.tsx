239e182817acc67903013bef719a626f1c863bcf
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-07-24 21:20:41 +0300

    feat : autoformat

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,28 +3,31 @@ import { Inter as FontSans } from "next/font/google"
 
 import { cn } from "@/lib/utils"
 import { Metadata } from "next"
+import { ClerkProvider } from "@clerk/nextjs"
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 })
-export const Metadate : Metadata = {
-  title : "Livedocs",
-  description : "Your go-to collaborative editor"
+export const Metadate: Metadata = {
+  title: "Livedocs",
+  description: "Your go-to collaborative editor"
 }
 
-export default function RootLayout({ children }: {children : React.ReactNode}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body
-        className={cn(
-          "min-h-screen font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body
+          className={cn(
+            "min-h-screen font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
