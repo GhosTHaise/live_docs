429803b240e3865ae0174dee6ae47effc0bd2bb8
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-07-24 21:22:39 +0300

    setting clerk interface

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter as FontSans } from "next/font/google"
 import { cn } from "@/lib/utils"
 import { Metadata } from "next"
 import { ClerkProvider } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -16,7 +17,15 @@ export const Metadate: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: {
+          colorPrimary: "#3371FF",
+          fontSize: "16px"
+        }
+      }}
+    >
       <html lang="en" suppressHydrationWarning>
         <head />
         <body
