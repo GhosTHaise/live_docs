900a061d0467569a17e5dd906b196fcba3d7d8be
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-07-29 21:01:12 +0300

    feat : get users associated to current room

--- app/provider.tsx
+++ app/provider.tsx
@@ -6,10 +6,18 @@ import {
     ClientSideSuspense,
 } from "@liveblocks/react/suspense";
 import Loader from '@/components/Loader';
+import { getClerkUser } from '@/lib/actions/user.action';
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
     return (
-        <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
+        <LiveblocksProvider
+            authEndpoint="/api/liveblocks-auth"
+            resolveUsers={async ({ userIds }) => {
+                const users = await getClerkUser({ userIds });
+
+                return users;
+            }}
+        >
             <RoomProvider id="my-room">
                 <ClientSideSuspense fallback={<Loader />}>
                     {children}
