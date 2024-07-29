987268b80c483ea7b73f59099b999d2a4c6325e8
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-07-29 20:36:51 +0300

    collaborative room

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,10 +1,11 @@
+import CollaborativeRoom from '@/components/collaborativeRoom'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-
-    </div>
+    <main className='flex w-full flex-col items-center'>
+      <CollaborativeRoom />
+    </main>
   )
 }
 
