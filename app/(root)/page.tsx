e00904f278b45c310f99a7c558623f1d81152c46
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-07-29 20:34:35 +0300

    feat : new document

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,15 @@
 import AddDocumentBtn from '@/components/addDocumentBtn';
 import Header from '@/components/header'
 import { SignedIn, UserButton } from '@clerk/nextjs'
-import { currentUser } from '@clerk/nextjs/server';
-import { redirect } from 'next/dist/server/api-utils';
+import { currentUser } from '@clerk/nextjs/server'
 import Image from 'next/image';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
 const Home = async () => {
   const clerkUser = await currentUser();
-  if (!clerkUser) redirect("/sign-in")
+  if (!clerkUser) redirect("/sign-in");
+
   const documents = [];
   return (
     <main className='home-container'>
@@ -19,30 +20,29 @@ const Home = async () => {
             <UserButton />
           </SignedIn>
         </div>
-
-        {
-          documents.length > 0 ? (
-            <div>
-
-            </div>
-          ) : (
-            <div className='document-list-empty'>
-              <Image
-                src="/assets/icons/doc.svg"
-                width={40}
-                height={40}
-                alt='Document'
-                className='mx-auto'
-              />
-
-              <AddDocumentBtn
-                userId={clerkUser!.id}
-                email={clerkUser!.emailAddresses[0].emailAddress}
-              />
-            </div>
-          )
-        }
       </Header>
+      {
+        documents.length > 0 ? (
+          <div>
+
+          </div>
+        ) : (
+          <div className='document-list-empty'>
+            <Image
+              src="/assets/icons/doc.svg"
+              width={40}
+              height={40}
+              alt='Document'
+              className='mx-auto'
+            />
+
+            <AddDocumentBtn
+              userId={clerkUser.id}
+              email={clerkUser!.emailAddresses[0].emailAddress}
+            />
+          </div>
+        )
+      }
     </main >
   )
 }
