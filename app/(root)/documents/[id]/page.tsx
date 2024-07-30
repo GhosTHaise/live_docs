07660972b3eb80268e9180099725cfa2275b75f3
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-07-30 22:27:54 +0300

    show connected user

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,10 +1,29 @@
 import CollaborativeRoom from '@/components/collaborativeRoom'
+import { getDocument } from '@/lib/actions/room.action';
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const page = () => {
+const page = async ({ params: { id } }: SearchParamProps) => {
+  const clerkUser = await currentUser();
+  if (!clerkUser) redirect('/sign-in');
+
+  const room = await getDocument({
+    roomId: id,
+    userId: clerkUser.emailAddresses[0].emailAddress
+  });
+
+  if (!room) redirect("/");
+
+  // TODO Ask the permissions of the user to access the document
+
   return (
     <main className='flex w-full flex-col items-center'>
-      <CollaborativeRoom />
+      <CollaborativeRoom
+        roomId={id}
+        roomMetadata={room.metadata}
+
+      />
     </main>
   )
 }
