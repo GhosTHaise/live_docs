943bc96cde28fc46d703b98f7d82e162723c94df
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-08-16 22:19:18 +0300

    feat : try to implement mention

--- app/provider.tsx
+++ app/provider.tsx
@@ -5,9 +5,12 @@ import {
     ClientSideSuspense,
 } from "@liveblocks/react/suspense";
 import Loader from '@/components/loader';
-import { getClerkUser } from '@/lib/actions/user.action';
+import { getClerkUser, getDocumentUsers } from '@/lib/actions/user.action';
+import { useUser } from '@clerk/nextjs';
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
+    const { user: clerkUser } = useUser();
+
     return (
         <LiveblocksProvider
             authEndpoint="/api/liveblocks-auth"
@@ -15,6 +18,15 @@ const Provider = ({ children }: { children: React.ReactNode }) => {
                 const users = await getClerkUser({ userIds });
 
                 return users;
+            }}
+            resolveMentionSuggestions={async ({ text, roomId }) => {
+                const roomUsers = await getDocumentUsers({
+                    roomId,
+                    currentUser: clerkUser?.emailAdresses[0].emailAdress!,
+                    text
+                });
+
+                return roomUsers;
             }}
         >
             <ClientSideSuspense fallback={<Loader />}>
