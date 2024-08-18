29a9b1cc0be83f4bb7b20c4d89b0dc2ac7f71726
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-08-18 20:48:40 +0300

    feat : add email input

--- components/sharedModal.tsx
+++ components/sharedModal.tsx
@@ -12,6 +12,8 @@ import {
 } from "@/components/ui/dialog"
 import { Button } from './ui/button';
 import Image from 'next/image';
+import { Label } from './ui/label';
+import { Input } from './ui/input';
 
 const SharedModal = ({ roomId,
     collaborators,
@@ -54,9 +56,22 @@ const SharedModal = ({ roomId,
                     <DialogDescription>
                         Select which user can view and edit this document
                     </DialogDescription>
-
-
                 </DialogHeader>
+                <Label htmlFor='email' className='mt-6 text-blue-100' >
+                    Email address
+                </Label>
+
+                <div className='flex items-center gap-3'>
+                    <div className='flex flex-1 rounded-md bg-dark-400'>
+                        <Input
+                            id='email'
+                            placeholder='Enter email address'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className='share-input'
+                        />
+                    </div>
+                </div>
             </DialogContent>
         </Dialog>
     )
