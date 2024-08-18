f06e9f5c6820c786035121e3dae401f5cee2a945
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-08-18 21:49:07 +0300

    user type select input

--- components/useTypeSelector.tsx
+++ components/useTypeSelector.tsx
@@ -1,12 +1,36 @@
 import React from 'react'
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
 
 const UseTypeSelector = ({
     userType,
     setUserType,
     onClickHandler
 }: UserTypeSelectorParams) => {
+    const accessChangeHandler = (type: UserType) => {
+        setUserType(type);
+        onClickHandler && onClickHandler(type);
+    }
+
     return (
-        <div>UseTypeSelector</div>
+        <Select
+            value={userType}
+            onValueChange={(type: UserType) => accessChangeHandler(type)}
+        >
+            <SelectTrigger className="shad-select">
+                <SelectValue placeholder="Theme" />
+            </SelectTrigger>
+            <SelectContent className='border-none bg-dark-200'>
+                <SelectItem value="viewer" className='shad-select-item'>Can view</SelectItem>
+                <SelectItem value="editor" className='shad-select-item'>Can edit</SelectItem>
+            </SelectContent>
+        </Select>
+
     )
 }
 
